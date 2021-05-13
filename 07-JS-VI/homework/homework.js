4f0ee35a9c571f5cf0ee599bc1cf7e205c0b35ca
Vanesa Rodriguez <vanesamr1993@gmail.com>
2021-05-13 12:45:28 -0300

    CP commiteado por vanesamr a las 1620920728105

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre[0].toUpperCase() + nombre.substring(1,nombre.length);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+ cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb (n1, n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +25,18 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  function sumar(elValorHastaElMomento, elValorActual) {
+    return elValorHastaElMomento + elValorActual;
+  }
+  var sumaDeElementos = numeros.reduce(sumar, 0);
+  cb(sumaDeElementos);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(cb)
 }
 
 function map(array, cb) {
@@ -35,12 +44,23 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoArray = array.map(cb)
+  return nuevoArray
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  function empiezaConA(str) {
+    if(str[0]==="a") {
+      return true
+    }
+    else
+    return false
+  }
+
+  return array.filter(empiezaConA)
 }
 
 // No modificar nada debajo de esta línea
